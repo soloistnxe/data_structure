6358a55c52399559d0a050c3d56097f10b6e0bab
soloist <quyuxuan0820@163.com>
2019-07-17 07:37:38 +0800

    增加栈

--- src/Stack/ArrayStackDemo.java
+++ src/Stack/ArrayStackDemo.java
@@ -1,8 +1,47 @@
 package Stack;
 
+import java.util.Scanner;
+
 public class ArrayStackDemo {
     public static void main(String[] args) {
-
+        //测试一下ArrayStack是否正确
+        //先创建一个ArrayStack对象-》表示栈
+        ArrayStack stack = new ArrayStack(4);
+        String key;
+        boolean loop = true;//控制是否退出菜单
+        Scanner scanner = new Scanner(System.in);
+        while (loop){
+            System.out.println("show: 表示显示栈");
+            System.out.println("exit: 退出程序");
+            System.out.println("push: 表示添加数据到栈（入栈）");
+            System.out.println("show: 表示从栈取出数据（出栈）");
+            System.out.println("请输入你的选择");
+            key = scanner.next();
+            switch (key){
+                case "show":
+                    stack.list();
+                    break;
+                case "push":
+                    System.out.println("请输入一个数");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case "pop":
+                    try {
+                        int res = stack.pop();
+                        System.out.println("出栈的数据是"+res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "exit":
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 //定义一个ArrayStack 表示栈
@@ -27,7 +66,7 @@ class ArrayStack{
     public void push(int value){
         //先判断栈是否满
         if(isFull()){
-            System.out.println();
+            System.out.println("栈满");
             return;
         }
         top++;
